63a70030c084b9fdf32434560c14e7bffc726858
NikeOumi <kilur2003@yahoo.co.jp>
2014-04-03 14:20:51 +0900

    スライドショー機能完成、サンプルピクチャ追加

--- js/common.js
+++ js/common.js
@@ -1,8 +1,29 @@
 //スライドショー
-$(document).ready(function() {
-	$("#slider").rhinoslider({
-		effect: "explode",
-		easing: "easeInQuad"
-		});
-});
+			$(document).ready(function() {
+				$('#slider').rhinoslider({
+					controlsPlayPause: false,
+					effect: 'explode',
+					showTime: 5000,
+					autoPlay: true
+				});
+				
+			});
+/* var timer = false;
+$(window).resize(function() {
+    if(timer) {
+        return;
+    } else {
+        timer = true;
+        setTimeout(function() {
+            if($('#rhinoslider').data('rhinoslider')) {
+                $('#rhinoslider').data('rhinoslider').uninit();
+            }
+            $('#rhinoslider').rhinoslider({
+                effect: 'slide'
+            });
+            timer = false;
+        }, 200);
+    }
+}); */
+
 //スライドショー
