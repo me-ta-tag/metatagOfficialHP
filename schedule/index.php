c014a8f41b5815a139498acab9470e4c192bcf01
LastyRain <rastyrein.ken.091012@gmail.com>
2014-04-10 14:39:07 +0900

    marge

--- schedule/index.php
+++ schedule/index.php
@@ -99,7 +99,7 @@
 		style=" border-width:0 " width="600" height="375" frameborder="0" scrolling="no">
 		</iframe> -->
 	</div>
-	 <div id='calendar'><!--↓にアップロードしたPHPファイルの絶対パスを入れる--> http://localhost/php/gcalendar-wrapper.php? 
+	 <div id='calendar'><!--↓にアップロードしたPHPファイルの絶対パスを入れる--> <!--http://localhost/php/gcalendar-wrapper.php? -->
 		<iframe 	
 		 src="../php/gcalendar-wrapper.php?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=4uld363ldgo0692u5nvk9qegqo%40group.calendar.google.com&amp;color=%236B3304&amp;ctz=Asia%2FTokyo"
 		 style=" border-width:0 " width="300" height="400" frameborder="0" scrolling="no">
