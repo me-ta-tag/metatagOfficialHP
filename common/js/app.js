8b0397418510133815de4dfd5bd0603f025921b2
Kentaro Hagiwara <rastyrein.ken.091012@gmail.com>
2014-12-08 18:33:58 +0900

    gitignore

---
+++ common/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by LastyRain on 2014/12/08.
+ */
+$(function(){
+
+    // Model
+    var Member = Backbone.Model.extend();
+
+    var member = new Member;
+    console.log(member);
+});
