ea7f6b7a25eb02504d95f5ca45929d866ed3ed96
sogawa <juukawa319@gmail.com>
2014-12-12 18:08:52 +0900

    無駄にdivに囲まれてappendされるけど、一応動くのでプッシュ

--- common/js/app.js
+++ common/js/app.js
@@ -7,5 +7,51 @@ $(function(){
     var Member = Backbone.Model.extend();
 
     var member = new Member;
-    console.log(member);
+
+
+/* Works-Page */
+	// Model
+    var WorkCard = Backbone.Model.extend({
+
+    });
+    // Collection
+    var WorkCards = Backbone.Collection.extend({
+    	model: WorkCard
+    });
+
+    // View
+    var WorkCardView = Backbone.View.extend({
+    	template: _.template($('#works_card_template').html()),
+		render: function(){
+			var tmp = this.template(this.model.toJSON());
+			this.$el.html(tmp);
+			return this;
+		}
+    });
+
+    var WorkCardsView = Backbone.View.extend({
+    	render: function(){
+    		this.collection.each(function(workcard){
+    			var workcardView = new WorkCardView({
+    				model: workcard
+    			});
+    			this.$el.append(workcardView.render().el);
+    		}, this);
+    		return this;
+    	}
+    });
+
+    // Collection生成
+    var workcards = new WorkCards([
+    	{name: '作品1', detail: '詳細情報', link_title: 'title'},
+    	{name: '作品2', detail: '詳細情報', link_title: 'title'},
+    	{name: '作品3', detail: '詳細情報', link_title: 'title'}
+    ]);
+
+    var workcardsView = new WorkCardsView({
+    	collection: workcards
+    });
+
+    $('#cards').append(workcardsView.render().el);
+
 });
