62da30fbd97e537374940d856cf95a9308658802
sogawa <juukawa319@gmail.com>
2015-01-13 18:04:23 +0900

    Backbone.Routerを付けました index.htmlに全ページのHTMLを書いて、1ページを遷移していくHPにします

--- common/js/app.js
+++ common/js/app.js
@@ -8,4 +8,45 @@ $(function(){
 
     var member = new Member;
     console.log(member);
+
+
+    var Router = Backbone.Router.extend({
+    	
+    	routes: {
+	    	'': 'index',
+	    	'about': 'about',
+	    	'works': 'works',
+	    	'member': 'member'
+	    },
+
+	    index: function(){
+	    	console.log('index');
+
+	    },
+
+	    about: function(){
+	    	console.log('about');
+	    	$('.about-card').hide();
+
+	    },
+
+	    works: function(){
+	    	console.log('works');
+	    	$('.works-card').hide();
+
+	    },
+
+	    member: function(){
+	    	console.log('member');
+	    	$('.member-card').hide();
+
+	    }
+
+
+    });
+
+    var router = new Router();
+
+    Backbone.history.start();
+
 });
